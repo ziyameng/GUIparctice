d74f7039f4f59339b6989bfc466d253aa8d13428
Zia999 <92579255+Zia999@users.noreply.github.com>
2022-11-21 15:30:34 +0000

    jpanel

--- src/Main.java
+++ src/Main.java
@@ -4,40 +4,44 @@ import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
-        //JLabel =  a GUI display area for a string of text, an image, or both
-        ImageIcon image = new ImageIcon("mario.png");//4.import the icon image
-        Border border = BorderFactory.createLineBorder(Color.green, 3);//create a border
+        //JPanel = a GUI component that functions as a container to hold other components
+        ImageIcon icon = new ImageIcon("mario.png");
 
-        JLabel label = new JLabel();//1.create a label
-        label.setText("Bro, do you even code?");//2.set text of label
-        label.setIcon(image);//5.set image for the icon
-        label.setHorizontalTextPosition(JLabel.CENTER);// set text LEFT, CENTER, RIGHT of imageicon
-        label.setVerticalTextPosition(JLabel.TOP);//set text TOP, CENTER, BOTTOM of imageicon;
-        label.setForeground(new Color(0x00ff00)); //Color.green / new Color(0,0,0) => set color of text
-        label.setFont(new Font("MV Boli", Font.PLAIN, 20));// set font of text
-        label.setIconTextGap(-25); //set gap of text to image
-        label.setBackground(Color.black);// set background color
-        label.setOpaque(true);//display background color
-        label.setBorder(border);/// set a border of label not (image+text)
-        label.setVerticalAlignment(JLabel.CENTER); //set vertical position of icon+text within label
-        label.setHorizontalAlignment(JLabel.CENTER); //set horizontal position of icon+text within label
-//        label.setBounds(0,0,350,350);//set x,y position with frame as well as dimensions
+        JLabel label = new JLabel();
+        label.setText("Hi");
+        label.setIcon(icon);
+//        label.setVerticalAlignment((JLabel.BOTTOM));//2
+//        label.setHorizontalAlignment(JLabel.RIGHT);
+        label.setBounds(0,0,75,75);// #place components in this area og your panel container
 
 
+        JPanel redPanel = new JPanel();
+        redPanel.setBackground(Color.red);
+        redPanel.setBounds(0, 0, 250, 250);
+        redPanel.setLayout(null);
+
+        JPanel bluePanel = new JPanel();
+        bluePanel.setBackground(Color.blue);
+        bluePanel.setBounds(250, 0, 250, 250);
+        bluePanel.setLayout(null);// nothing displayed in this panel
+
+        JPanel greenPanel = new JPanel();
+        greenPanel.setBackground(Color.green);
+        greenPanel.setBounds(0, 250, 500, 250);
+        greenPanel.setLayout(null);
 
         //JFrame create a frame
         JFrame frame = new JFrame();
-        frame.add(label); //3.add label to frame
+        frame.add(redPanel);
+        frame.add(bluePanel);
+        frame.add(greenPanel);
 
-//        frame.setLayout(null);// for setBounds
+        bluePanel.add(label);//#
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//        frame.setSize(500, 500);
+        frame.setLayout(null);
+        frame.setSize(750, 750);
         frame.setVisible(true);
-        frame.pack();//adjust to accomondate the content automatically, pack should be used at the end u got all ur components ready
-
-
-
 
     }
 }
